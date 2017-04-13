a6cfae8c29049c9e72ed49ca0c131e01a26395f0
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-04-13 17:41:05 -0300

    test(Saint): testar ações realizadas na classe Saint.

---
+++ cavaleirosdozodiaco/SaintTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SaintTest {
+    @Test
+    public void vestirArmaduraDeixaArmaduraVestida() {
+   
+         // AAA
+         // 1. Arrange - Montagem dos dados de teste
+         Armadura armadura = new Armadura("Aquário", Categoria.OURO);
+         Saint saint = new Saint("Camus", armadura);
+         // 2. Act - Invocar ação a ser testada
+         saint.vestirArmadura();
+         // 3. Assert - Verificação dos resultados do teste
+         boolean resultado = saint.getArmaduraVestida();
+         assertEquals(true, resultado);
+    }
+    
+    @Test
+    public void naoVestirArmaduraDeixaArmaduraNaoVestida () {
+        Saint saint = new Saint ("Hyoga", new Armadura("Cisne", Categoria.BRONZE));
+        assertEquals(false, saint.getArmaduraVestida());
+    }
+    
+    @Test
+    public void aoCriarSaintGeneroENaoInformado () {
+        Saint saint = new Saint("Shaka", new Armadura ("Virgem", Categoria.OURO));
+        assertEquals(Genero.NAO_INFORMADO, saint.getGenero());
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
