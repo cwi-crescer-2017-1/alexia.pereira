1308f5240c2b257396d796e5028d4d9f6983a28e
Unknown <lecavsalexia@gmail.com>
2017-04-14 17:11:59 -0300

    feat(Saint): implementar ações referentes a vida do Saint.

--- cavaleirosdozodiaco/Saint.java
+++ cavaleirosdozodiaco/Saint.java
@@ -3,10 +3,13 @@ public class Saint {
     private Armadura armadura;
     private boolean armaduraVestida;
     private Genero genero = Genero.NAO_INFORMADO;
+    private StatusDeVida status = StatusDeVida.VIVO;
+    private double vida;
     
     public Saint(String nome, Armadura armadura) {
         this.nome = nome;
         this.armadura = armadura;
+        this.vida = 100.0;
     }
     
     public void vestirArmadura () {
@@ -24,4 +27,23 @@ public class Saint {
     public void setGenero (Genero genero) {
         this.genero = genero;
     }
+    
+    public StatusDeVida getStatus () {
+        return this.status;
+    }
+    
+    public double getVida () {
+        return this.vida;
+    }
+    
+    public void perderVida (double vida) {
+        this.vida = this.vida - vida;
+    }
+    
+    public Armadura getArmadura () {
+        return this.armadura;
+    }
+    
+    
+    
 }
