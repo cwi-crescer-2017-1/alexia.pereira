1448b92d4af525ae8ccf596bb4a5b09993d41b85
Unknown <lecavsalexia@gmail.com>
2017-04-17 20:30:41 -0300

    feat(Exceção): implementar InvalidParameterException.

---
+++ cavaleirosdozodiaco/InvalidParameterException.java
@@ -0,0 +1,6 @@
+public class InvalidParameterException extends Exception { 
+      //Constructor that accepts a message
+      public InvalidParameterException(String message) {
+         super(message);
+      }
+}
