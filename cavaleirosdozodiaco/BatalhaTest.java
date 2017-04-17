a76edde7a3608c9eb7a9c8f21859b073abfa7da6
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-04-17 15:06:31 -0300

    test(Batalha): adicionar testes pra outras possibilidades do método iniciar.

--- cavaleirosdozodiaco/BatalhaTest.java
+++ cavaleirosdozodiaco/BatalhaTest.java
@@ -4,24 +4,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class BatalhaTest {
-    
+
     @Test
-    public void iniciarVerificaCategoriasCorretamente () {
+    public void categoriaSaint1MaiorQueSaint2 () {
         Saint seiya = new Saint ("Seiya", new Armadura("Pegaso", Categoria.BRONZE));
         Saint marin = new Saint ("Marin", new Armadura("Águia", Categoria.PRATA));
         Batalha batalha = new Batalha (seiya, marin);
         batalha.iniciar();
         assertEquals(90.0, seiya.getVida(), 0.001);
+        assertEquals(100.0, marin.getVida(), 0.001);
     }
-    
+
+    @Test
+    public void iniciarVerificaEmpateDeCategoria () {
+        Saint alderbaran = new Saint ("Alderbaran", new Armadura("Touro", Categoria.OURO));
+        Saint mascaraMorte = new Saint ("Máscara da Morte", new Armadura("Câncer", Categoria.OURO));
+        Batalha batalha = new Batalha (alderbaran, mascaraMorte);
+        batalha.iniciar();
+        assertEquals(90.0, mascaraMorte.getVida(), 0.001);
+        assertEquals(100.0, alderbaran.getVida(), 0.001);
+    }
     
     @Test
-     public void iniciarVerificaEmpateDeCategoria () {
-        Saint seiya = new Saint ("Seiya", new Armadura("Pegaso", Categoria.BRONZE));
-        Saint marin = new Saint ("Marin", new Armadura("Águia", Categoria.BRONZE));
-        Batalha batalha = new Batalha (seiya, marin);
+    public void categoriaSaint2MaiorQueSaint1 () {
+        Saint ikki = new Saint ("Ikki", new Armadura("Fênix", Categoria.BRONZE));
+        Saint mascaraMorte = new Saint ("Máscara da Morte", new Armadura("Câncer", Categoria.OURO));
+        Batalha batalha = new Batalha (ikki, mascaraMorte);
         batalha.iniciar();
-        assertEquals(90.0, marin.getVida(), 0.001);
+        assertEquals(100.0, mascaraMorte.getVida(), 0.001);
+        assertEquals(90.0, ikki.getVida(), 0.001);
     }
+    
 
 }
