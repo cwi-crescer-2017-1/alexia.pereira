a90d725254a594281313cb7d6c1f7476677b578d
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-04-18 15:46:44 -0300

    refactor(adicionarGolpe): refatorar método adicionar golpe.

--- cavaleirosdozodiaco/Constelacao.java
+++ cavaleirosdozodiaco/Constelacao.java
@@ -1,27 +1,22 @@
 public class Constelacao  {
     private String nome;
     private Golpe [] golpes = new Golpe [3];
-   
+    private int pontoInsercao = 0;
+
     public Constelacao (String nome) {
         this.nome = nome;
     }
-    
+
     public String getNome () {
         return this.nome;
     }
-    
+
     public Golpe[] getGolpes () {
         return this.golpes;
     }
-    
+
     public void adicionarGolpe (Golpe golpe) {
-        int contador = this.golpes.length;
-        for (int i = 0; i < this.golpes.length; i++) {
-            if (golpes[i] == null) {
-                contador--;
-            }           
-        }
-        golpes[contador] = golpe; 
+            this.golpes[pontoInsercao++] = golpe;
     }
-    
+
 }
--- cavaleirosdozodiaco/ConstelacaoTest.java
+++ cavaleirosdozodiaco/ConstelacaoTest.java
@@ -5,12 +5,53 @@ import org.junit.Test;
 
 public class ConstelacaoTest {
     @Test
-    public void adicionarGolpeAdicionaGolpe () {
+    public void adicionarUmGolpe () {
         Constelacao constelacao = new Constelacao("Pégaso");
         Golpe golpe = new Golpe ("Meteoro de Pegaso", 100);
-        constelacao.adicionarGolpe(golpe);
+        constelacao.adicionarGolpe(new Golpe ("Meteoro de Pegaso", 100));
         assertEquals(golpe, constelacao.getGolpes()[0]);
+        assertNull(constelacao.getGolpes()[1]);
+        assertNull(constelacao.getGolpes()[2]);
     }
-    
-    
+
+    @Test
+    public void adicionarDoisGolpes () {
+        Constelacao constelacao = new Constelacao("Pégaso");
+        Golpe golpe1 = new Golpe ("Meteoro de Pegaso", 100);
+        constelacao.adicionarGolpe(golpe1);
+        assertEquals(golpe1, constelacao.getGolpes()[0]);
+        Golpe golpe2 = new Golpe ("Metéoro 2", 80);
+        constelacao.adicionarGolpe(new Golpe ("Metéoro 2", 80));
+        assertEquals(golpe2, constelacao.getGolpes()[1]);
+        assertNull(constelacao.getGolpes()[2]);
+    }
+
+    @Test
+    public void adicionarTresGolpes () {
+        Constelacao constelacao = new Constelacao("Pégaso");
+        Golpe golpe1 = new Golpe ("Meteoro de Pegaso", 100);
+        constelacao.adicionarGolpe(golpe1);
+        assertEquals(golpe1, constelacao.getGolpes()[0]);
+        Golpe golpe2 = new Golpe ("Metéoro 2", 80);
+        constelacao.adicionarGolpe(new Golpe ("Metéoro 2", 80));
+        assertEquals(golpe2, constelacao.getGolpes()[1]);
+        Golpe golpe3 = new Golpe ("Metéoro 3", 30);
+        constelacao.adicionarGolpe(new Golpe ("Metéoro 3", 30));
+        assertEquals(golpe3, constelacao.getGolpes()[2]);
+    }
+
+    @Test (expected = ArrayIndexOutOfBoundsException.class)
+    public void adicionarQuatroGolpes () {
+        Constelacao constelacao = new Constelacao("Pégaso");
+        Golpe golpe1 = new Golpe ("Meteoro de Pegaso", 100);
+        constelacao.adicionarGolpe(golpe1);
+        Golpe golpe2 = new Golpe ("Metéoro 2", 80);
+        constelacao.adicionarGolpe(new Golpe ("Metéoro 2", 80));
+        Golpe golpe3 = new Golpe ("Metéoro 3", 30);
+        constelacao.adicionarGolpe(new Golpe ("Metéoro 3", 30));
+        Golpe golpe4 = new Golpe ("Metéoro 4", 40);
+        constelacao.adicionarGolpe(new Golpe ("Metéoro 4", 40));
+
+    }
+
 }
