247610e0fda7899be0cd6cfa99116eae63e8da09
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-04-18 15:47:18 -0300

    feat(Getters): adicionar getters e modificar método equals.

--- cavaleirosdozodiaco/Golpe.java
+++ cavaleirosdozodiaco/Golpe.java
@@ -7,6 +7,27 @@ public class Golpe {
         this.fatorDano = fatorDano;
     }
     
+    public String getNome () {
+        return this.nome;
+    }
+    
+    public int getFatorDano () {
+        return this.fatorDano;
+    }
+    
+    
+    public boolean equals(Object object) {
+     Golpe outroGolpe = (Golpe)object;
+     return this.nome.equals(outroGolpe.getNome()) && this.fatorDano == outroGolpe.getFatorDano();
+     
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
 
