3c05bbde3bfe73e3c44198e05aaf06e0a29d60e8
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-04-28 14:41:36 -0300

    feat(exercicio): adicionar arquivo sql de resolução do exercício 1.

---
+++ modulo-02/exercicio01.sql
@@ -0,0 +1,29 @@
+-- 1
+SELECT * INTO CidadeAux FROM cidade;
+
+SELECT * FROM CidadeAux;
+
+-- 2
+TRUNCATE TABLE CidadeAux;
+
+INSERT INTO CidadeAux(idCidade, nome, UF) SELECT * FROM Cidade WHERE Cidade.UF = 'RS';
+
+-- 3
+CREATE TABLE produto (
+	id_produto INT NOT NULL,
+	nome_curto VARCHAR(15) NOT NULL,
+	nome_descritivo VARCHAR(100) NOT NULL,
+	data_criacao DATE NOT NULL,
+	local_no_estoque VARCHAR(30),
+	quantidade INT NOT NULL,
+	preco FLOAT NOT NULL,
+	CONSTRAINT PK_Produto PRIMARY KEY (id_produto)
+);
+
+--4
+INSERT INTO produto (id_produto, nome_curto, nome_descritivo, data_criacao, local_no_estoque, quantidade, preco)
+VALUES (1, 'Computador', 'Computador Dell', '17/04/28', 'A12', 3, 2800);
+
+INSERT INTO produto (id_produto, nome_curto, nome_descritivo, data_criacao, local_no_estoque, quantidade, preco)
+VALUES (2, 'Celular', 'Zenfone 2', '17/04/28', 'B20', 5, 1000);
+
