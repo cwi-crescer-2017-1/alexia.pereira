b1293f371e5750f797e1b6b92644c55670c67e9e
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-02 22:32:40 -0300

    feat(ex3): adicionar resolução do exercício 3.

--- modulo-02/Lista06.sql
+++ modulo-02/Lista06.sql
@@ -10,3 +10,26 @@ GROUP BY fn.PrimeiroNome ORDER BY TotalOcorrencia DESC;
 /* Liste o total de pedidos (quantidade e valor) realizados no mês de abril/2017. */
 SELECT COUNT(p.IdPedido) AS Quantidade, SUM(p.ValorPedido) AS ValorPedido
 FROM pedido p WHERE YEAR(DataPedido) = 2017 AND MONTH (DataPedido) = 4;
+
+-- 3
+/* Identifique qual o estado (coluna UF da tabela Cidade) possuí o maior número de clientes (tabela Cliente), 
+	liste também qual o Estado possuí o menor número de clientes. */
+
+
+SELECT temp.UF, temp.Quantidade FROM 
+	(SELECT ci.UF, COUNT(cl.idCliente) AS Quantidade FROM cliente cl INNER JOIN cidade ci ON 
+		ci.IdCidade = cl.IdCidade GROUP BY ci.UF
+	) AS temp
+WHERE temp.Quantidade IN
+	(SELECT MAX(temp2.Quantidade) FROM 
+		(SELECT COUNT(cl.idCliente) AS Quantidade FROM cliente cl INNER JOIN
+			cidade ci ON ci.IdCidade = cl.IdCidade GROUP BY ci.UF
+		) AS temp2
+	)
+OR temp.Quantidade IN
+(SELECT MIN(temp3.Quantidade) FROM 
+		(SELECT COUNT(cl.idCliente) AS Quantidade FROM cliente cl INNER JOIN
+			cidade ci ON ci.IdCidade = cl.IdCidade GROUP BY ci.UF
+		) AS temp3
+	)
+ ORDER BY Quantidade ASC;
