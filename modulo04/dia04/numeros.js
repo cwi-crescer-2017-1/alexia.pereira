aac16aceb8c272f46a4636eae69fc0a086390945
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-16 14:07:12 -0300

    feat(removerToFixed): adicionar solução alternativa ao toFixed.

--- modulo04/dia04/numeros.js
+++ modulo04/dia04/numeros.js
@@ -1,4 +1,10 @@
-Number.prototype.arredonda = function (casasDecimais) {
+// Number.prototype.arredonda = function (casasDecimais) {
+//   casasDecimais = casasDecimais || 2;
+//   return Number(this.toFixed(casasDecimais));
+// };
+
+
+Number.prototype.arredondar = function(casasDecimais) {
   casasDecimais = casasDecimais || 2;
-  return Number(this.toFixed(casasDecimais));
+ return Math.round(this * Math.pow(10, casasDecimais)) / (Math.pow(10, casasDecimais));
 };
