9829ff7b6a36d62d7944113bb468d598bf496b40
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-17 14:12:57 -0300

    feat(exercicio02): adicionar status dos pokemons.

--- modulo04/dia04/js/pokeapi.js
+++ modulo04/dia04/js/pokeapi.js
@@ -10,8 +10,7 @@ btnPesquisar.onclick = function() {
   procuraPorNumero(valor);
 }
 
-
-
+// fetch('http://pokeapi.co/api/v2/stat/6/').then(response => response.json()).then(json => console.log(json));
 // fetch("http://pokeapi.co/api/v2/pokemon/5/").then(response => response.json())
 //   .then(json => {
 //     console.log("JSON 1", json)
@@ -24,20 +23,38 @@ btnPesquisar.onclick = function() {
 //           })
 //       })
 //   })
+
+  function procurarStatus (obj, div) {
+    var b =  obj.map(function (e) {
+      let a = {name: e.stat.name, base_stat: e.base_stat};
+      return a;
+    });
+  b.forEach(function(e) {
+    let progress = document.createElement('progress');
+    texto = document.createElement('h3').appendChild(document.createTextNode(e.name));
+    progress.value = e.base_stat;
+    progress.max = 100;
+    div.append(texto);
+    div.append(progress);
+  });
+}
+
+
 function procuraPorNumero (input) {
   let url = "http://pokeapi.co/api/v2/pokemon/" + input;
   fetch(url)
   .then(response => response.json())
   .then(json => {
     console.log(json);
+    let divPokemon = document.createElement('div');
+    divPokemon.className = "pokemon";
     let div = document.getElementById('detalhesPokemon');
     let h1 = document.createElement('h1');
     h1.appendChild(document.createTextNode(json.name + " - " + json.id));
-    div.append(h1);
-    // div.appendChild(document.createTextNode(json.id))
+    divPokemon.append(h1);
     let img = document.createElement('img');
     img.src = json.sprites.front_default;
-    div.append(img);
+    divPokemon.append(img);
     let ul = document.createElement('ul');
     let tipos = json.types;
     tipos.forEach(function (e) {
@@ -45,6 +62,8 @@ function procuraPorNumero (input) {
       li.appendChild(document.createTextNode(e['type'].name));
       ul.appendChild(li);
     });
-    div.append(ul);
+    divPokemon.append(ul);
+    procurarStatus(json.stats, divPokemon);
+    div.append(divPokemon);
   })
 }
