f904bc2bd228416a4a780007ac50b3e7f3221c6d
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-19 00:26:03 -0300

    feat(lista01): adicionar exercicios 1, 2 e 3 da lista 1.
    TODO: Style.

---
+++ modulo05-angular/dia 02/js/lista01.js
@@ -0,0 +1,71 @@
+var aplicacao = angular.module('ex02', []);
+
+aplicacao.filter('mascada', function() {
+  return function mascada(nome) {
+    return nome.replace(/(nunes)/ig, '$ $1 $');
+  }
+})
+
+aplicacao.filter('listarAulas', function() {
+  return function listarAulas(aula) {
+    var str = "" + aula.numeroAula;
+    var pad = "000"
+    var num = pad.substring(0, pad.length - str.length) + str
+    let formatacao = num + " - " + aula.nomeAula;
+    return formatacao;
+  }
+});
+
+aplicacao.controller('Controller', ['$scope', function(model) {
+  let instrutores = [{
+    nome: 'Pedro (PHP)',
+    aula: [{
+      numero: 3,
+      nome: 'HTML e CSS'
+    }]
+  },
+  {
+    nome: 'Zanatta',
+    aula: [{
+      numero: 5,
+      nome: 'AngularJS'
+    }]
+  },
+  {
+    nome: 'Bernardo',
+    aula: [{
+      numero: 1,
+      nome: 'OO'
+    },
+    {
+      numero: 4,
+      nome: 'Javascript'
+    }
+  ]
+},
+{
+  nome: 'Nunes',
+  aula: [{
+    numero: 2,
+    nome: 'Banco de Dados I'
+  }]
+}
+];
+model.instrutores = instrutores;
+
+function converterPraAulas (instrutores) {
+  let aulas = [];
+  instrutores.map(function(i) {
+    for (let cont = 0; cont < i.aula.length; cont ++) {
+      let c = {nomeAula: i.aula[cont].nome, numeroAula: i.aula[cont].numero, nomeInstrutor: i.nome};
+      aulas.push(c);
+    }
+  }
+);
+console.log(aulas);
+return aulas;
+};
+
+model.aulas = converterPraAulas(model.instrutores);
+
+}]);
