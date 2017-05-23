8f6f18171ad9a1619a13f1b871f74bffe3ef8b3d
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-23 15:12:15 -0300

    refactor(crudAula): remover códigos desnecessários para o crud de aulas.

--- modulo05-angular/Route/js/controller.js
+++ modulo05-angular/Route/js/controller.js
@@ -13,9 +13,6 @@ app.config(function ($routeProvider) {
   .otherwise({redirectTo: '/aulas'});
 });
 
-var idAula = 0;
-var idInstrutor = 0;
-
 app.filter('aulasDosInstrutores', function() {
   return function aulasDosInstrutores(idAula, aulas) {
     return aulas.filter(aula => aula.id === idAula).map(e => e.nome);
@@ -24,18 +21,17 @@ app.filter('aulasDosInstrutores', function() {
 
 app.controller('AulasController', function ($scope, $routeParams, aulaService) {
   // AULAS
-
   $scope.id = $routeParams.idUrl;
 
   $scope.update = update;
   $scope.create = create;
   $scope.delete = deletar;
   // Ações executadas quando criar a controller
-  // findById($scope.id);
   list(); // listar aulas
 
   // Funções internas
   function create(aula) {
+    if (formInvalido(false)) return;
     aulaService.create(aula).then(function () {list();});
   };
 
@@ -52,6 +48,7 @@ app.controller('AulasController', function ($scope, $routeParams, aulaService) {
   }
 
   function update(aula) {
+    if (formInvalido(true)) return;
     aulaService.update(aula).then(function () {
       list();
       $scope.showForm = false;
@@ -65,161 +62,62 @@ app.controller('AulasController', function ($scope, $routeParams, aulaService) {
     })
   }
 
-
-  // $scope.aulaPraDeletar = undefined;
-  // $scope.showForm = false;
-  // $scope.aulasendoUtilizada = false;
-  //
-  // $scope.incluirAula = function (nomeAula) {
-  //   if ($scope.meuForm.$invalid || !$scope.meuForm.$valid) {
-  //     return;
-  //   }
-  //   let novaAula = {id: ++idAula, nome: nomeAula };
-  //   $scope.aulas.push(angular.copy(novaAula));
-  //   $scope.novaAula = "";
-  //   alert("Aula incluída com sucesso");
-  // }
-  //
-  // $scope.aulaJaCadastrada = function(nomeAula, update) {
-  //   // if($scope.aulaselecionada($scope.aulas)) {
-  //   let validade = $scope.aulas.filter(aula => aula.nome === nomeAula).length > 0;
-  //   if (!update) {
-  //     $scope.meuForm.$invalid = validade;
-  //   } else {
-  //     $scope.formUpdateAula.$invalid = validade;
-  //   }
-  //   return validade;
-  //   // }
-  // };
-  //
   $scope.aulaSelecionada = function (aulaS) {
     $scope.aulaS = angular.copy(aulaS);
     let taSelecionada = typeof aulaS !== 'undefined';
     $scope.showForm = taSelecionada;
     return taSelecionada;
   };
-  //
-  // $scope.updateAula = function (nomeNovaAula) {
-  //   $scope.aulaJaCadastrada(nomeNovaAula, true);
-  //   if ($scope.formUpdateAula.$invalid || !$scope.formUpdateAula.$valid) {
-  //     return;
-  //   }
-  //   $scope.aulas.nome = nomeNovaAula;
-  //   $scope.novoNomeAula = "";
-  //   alert("Aula atualizada com sucesso");
-  // }
-  //
-  // $scope.deletarAula = function (aulaPraDeletar) {
-  //   let aulaSendoUtilizada = $scope.instrutores.filter(i => i.aula.includes(aulaPraDeletar.id)).length > 0;
-  //   $scope.aulasendoUtilizada = aulaSendoUtilizada;
-  //   if (aulaSendoUtilizada) {
-  //     return;
-  //   }
-  //   let i = aulas.indexOf(aulaPraDeletar);
-  //   aulas.splice(i, 1);
-  //   alert("Aula deletada com sucesso");
-  // }
-  //
-  // $scope.aulaNaoEstaSendoUtilizada = function () {
-  //   $scope.aulasendoUtilizada = false;
-  // }
+
+  function formInvalido (update) {
+    return update ? $scope.formUpdateAula.$invalid || !$scope.formUpdateAula.$valid
+    : $scope.meuForm.$invalid || !$scope.meuForm.$valid;
+  }
+
 });
 
-app.controller('InstrutoresController', function ($scope) {
+app.controller('InstrutoresController', function ($scope, $routeParams, instrutorService) {
 
   // INSTRUTORES
   $scope.showFormI = false;
-  $scope.instrutorS = $scope.instrutores[0];
-  $scope.selecionado = [];
-  $scope.instrutorSendoUtilizado = false;
+  $scope.id = $routeParams.idUrl;
 
-  $scope.incluirInstrutor = function (novoInstrutor) {
-    if ($scope.meuFormI.$invalid || !$scope.meuFormI.$valid) {
-      return;
-    }
-    novoInstrutor.aula.sort(ordenarAulas);
-    novoInstrutor.id = ++idInstrutor;
-    novoInstrutor.dandoAula = novoInstrutor.dandoAula || false;
-    novoInstrutor.urlFoto = novoInstrutor.urlFoto || "https://media.lovemondays.com.br/logos/e3b058/cwi-software-original.png";
-    $scope.instrutores.push(angular.copy(novoInstrutor));
-    alert("Instrutor incluído com sucesso");
-    // $scope.novoInstrutor = {};
-  }
+  $scope.update = update;
+  $scope.create = create;
+  $scope.delete = deletar;
+  // Ações executadas quando criar a controller
+  // findById($scope.id);
+  list();
 
-  $scope.aulasDosInstrutores = function (idAula) {
-    return $scope.aulas.filter(aula => aula.id === idAula).map(e => e.nome).shift();
+  // Funções internas
+  function create(instrutor) {
+    instrutorService.create(instrutor).then(function() { list(); });
   };
 
-  $scope.selecaoAlternada = function (aula) {
-    var idx = $scope.selecionado.indexOf(aula);
-    if (idx > -1) {
-      $scope.selecionado.splice(idx, 1);
-    }
-    else {
-      $scope.selecionado.push(aula);
-    }
-    $scope.novoInstrutor.aula = $scope.selecionado.map(aula => aula.id);
+  function findById(id) {
+    instrutorService.findById(id).then(function (response) {
+      $scope.instrutor = response.data;
+    });
   };
 
-  $scope.instrutorJaCadastrado = function(nomeInstrutor) {
-    // if($scope.instrutorSelecionado($scope.instrutorS)) {
-    let validade = $scope.instrutores.filter(instrutor => {
-      if (typeof $scope.instrutorS === 'undefined' || $scope.instrutorS === null) return false;
-      return instrutor.id !== $scope.instrutorS.id && instrutor.nome === nomeInstrutor
-    }).length > 0;
+  function list() {
+    instrutorService.list().then(function (response) {
+      $scope.instrutores = response.data;
+    });
+  }
 
-    $scope.meuFormI.$invalid = $scope.meuFormI.$invalid || validade;
-    return validade;
-    // }
+  function update(instrutor) {
+    instrutorService.update(instrutor).then(function () {
+      list();
+      $scope.showForm = false;
+    });
   };
 
-  $scope.emailJaCadastrado = function (emailInstrutor) {
-    let validade = $scope.instrutores.filter(instrutor => {
-      if (typeof $scope.instrutorS === 'undefined' || $scope.instrutorS === null) return false;
-      return instrutor.id !== $scope.instrutorS.id && instrutor.email === emailInstrutor
-    }).length > 0;
-    $scope.meuFormI.$invalid = $scope.meuFormI.$invalid || validade;
-    return validade;
-
+  function deletar (instrutor) {
+    if (typeof aula === 'undefined') return;
+    instrutorService.delete(instrutor).then(function () {
+      list();
+    })
   }
 
-  $scope.instrutorSelecionado = function (instrutorS) {
-    let taSelecionado = typeof instrutorS !== 'undefined';
-    $scope.showFormI = taSelecionado;
-    $scope.novoInstrutor = angular.copy(instrutorS);
-    return taSelecionado;
-  };
-
-  $scope.atualizarInstrutor = function (novoInstrutor) {
-    if ($scope.meuFormI.$invalid || !$scope.meuFormI.$valid) {
-      return;
-    }
-    novoInstrutor.aula.sort(ordenarAulas);
-    let i = $scope.instrutores.indexOf($scope.instrutorS);
-    $scope.instrutores[i] = novoInstrutor;
-    $scope.novoInstrutor = {};
-    $scope.showFormI = false;
-    alert("Instrutor atualizado com sucesso");
-  }
-
-  $scope.deletarInstrutor = function (instrutorParaDeletar) {
-    let sendoUtilizado = instrutorParaDeletar.dandoAula;
-    if(!sendoUtilizado) {
-      $scope.instrutores = $scope.instrutores.filter(instrutor => instrutor.id !== instrutorParaDeletar.id);
-      alert("Instrutor deletado com sucesso");
-    }
-    $scope.instrutorSendoUtilizado = sendoUtilizado;
-
-  }
-
-  $scope.instrutorNaoEstaSendoUtilizado = function () {
-    $scope.instrutorSendoUtilizado = false;
-  }
-
-  let ordenarAulas = function(aula1,aula2) {
-    if (typeof $scope.aulas[aula1] === 'undefined' || typeof $scope.aulas[aula2] === 'undefined') return 0;
-    if($scope.aulas[aula1].nome.toLowerCase() > $scope.aulas[aula2].nome.toLowerCase()) return 1;
-    if($scope.aulas[aula1].nome.toLowerCase() < $scope.aulas[aula2].nome.toLowerCase()) return -1;
-    return 0;
-  }
 });
