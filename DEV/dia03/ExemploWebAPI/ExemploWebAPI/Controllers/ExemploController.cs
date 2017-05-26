1d7058b40c90f8ebae109707b9ada15311ec44a8
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-26 17:30:16 -0300

    feat(dia03): adicionar exemplos feitos em aula.

---
+++ DEV/dia03/ExemploWebAPI/ExemploWebAPI/Controllers/ExemploController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Mvc;
+using ExemploWebAPI.Models;
+
+namespace ExemploWebAPI.Controllers
+{
+    public class ExemploController : ApiController
+    {
+        private static List<Heroi> herois = new List<Heroi>();
+
+        private object objetoLock = new object();
+
+        private static int IdS = 0;
+
+        public List<Heroi> Get(string nome = null, int? id = null)
+        {
+            return herois.Where(h =>
+                (id == null || id == h.Id) &&
+                (nome == null || nome == h.Nome))
+                .ToList();
+
+        }
+
+        public IHttpActionResult Post(Heroi heroi)
+        {
+            if (heroi == null)
+            {
+                return BadRequest();
+            }
+
+            else
+            {
+                lock (objetoLock)
+                {
+                    heroi.Id = ++IdS;
+                    herois.Add(heroi);
+                    return Ok(heroi);
+                }
+            }
+        }
+    }
+
+}
---
+++ DEV/dia03/ExemploWebAPI/ExemploWebAPI/Models/Heroi.cs
@@ -0,0 +1,10 @@
+﻿namespace ExemploWebAPI.Models
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Poder Poder { get; set; }
+    }
+    
+}
