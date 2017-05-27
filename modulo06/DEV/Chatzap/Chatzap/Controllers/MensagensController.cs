6fb3073aefe29d3038567a8af808e043c3003ab7
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-26 21:55:24 -0300

    feat(Mensagem): adicionar backend de mensagens com métodos Get e Post.

---
+++ modulo06/DEV/Chatzap/Chatzap/Controllers/MensagensController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Chatzap.Models;
+
+namespace Chatzap.Controllers
+{
+    public class MensagensController : ApiController
+    {
+        private static int incrementavel = 1;
+        private static object @lock = new object();
+        private static List<Mensagem> mensagens = new List<Mensagem>();
+
+        public IEnumerable<Mensagem> Get()
+        {
+            return mensagens;
+        }
+
+        public IHttpActionResult Post(Mensagem mensagem)
+        {
+            if (mensagem == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                lock (@lock)
+                {
+                    mensagens.Add(mensagem);
+                    mensagem.Id = incrementavel++;
+                    mensagem.DataMensagem = DateTime.Now;
+                }
+
+                return Ok(mensagem);
+            }
+        }
+
+
+    }
+}
---
+++ modulo06/DEV/Chatzap/Chatzap/Models/Mensagem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chatzap.Models
+{
+    public class Mensagem
+    {
+        public int Id { get; set; }
+        public string texto;
+        public DateTime DataMensagem;
+        public Usuario usuario;
+    }
+}
