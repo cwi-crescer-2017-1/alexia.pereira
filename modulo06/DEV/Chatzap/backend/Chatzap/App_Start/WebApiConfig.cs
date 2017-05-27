cd3ef78125a63b75c20fad1ab4e6a3cce0ac71af
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-26 23:40:01 -0300

    feat(cors): adicionar cors ao projeto.

--- modulo06/DEV/Chatzap/backend/Chatzap/App_Start/WebApiConfig.cs
+++ modulo06/DEV/Chatzap/backend/Chatzap/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Chatzap
 {
@@ -13,6 +14,7 @@ namespace Chatzap
 
             // Rotas da API da Web
             config.MapHttpAttributeRoutes();
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
