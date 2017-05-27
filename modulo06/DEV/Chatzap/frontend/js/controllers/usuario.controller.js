053ecc762698398531fcabdad9caaf41c83ed899
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-27 17:35:53 -0300

    feat(usuario): adicionar método de entrada na conversa.

--- modulo06/DEV/Chatzap/frontend/js/controllers/usuario.controller.js
+++ modulo06/DEV/Chatzap/frontend/js/controllers/usuario.controller.js
@@ -1,6 +1,6 @@
 angular.module('app').controller('UsuariosController', function ($scope, usuarioService) {
 
-  $scope.create = create;
+  $scope.entrarNaConversa =  entrarNaConversa;
   list();
 
   // Funções internas
@@ -9,6 +9,14 @@ angular.module('app').controller('UsuariosController', function ($scope, usuario
       list();
     });
   };
+
+  function entrarNaConversa () {
+    var userName = prompt("Digite seu Nome");
+    var foto = prompt("Digite uma URL pública pra sua foto");
+    var usuario = {Id: 0, Nome: userName, Foto: foto};
+    $scope.usuario = usuario;
+    create(usuario);
+  }
 
   function list() {
     usuarioService.list().then(function (response) {
