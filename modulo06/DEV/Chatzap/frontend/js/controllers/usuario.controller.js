5830c4e35f44fc959371a5f5a393a2eb1e697a39
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-28 15:52:43 -0300

    feat(localStorage): salvar informações do usuário no local storage.

--- modulo06/DEV/Chatzap/frontend/js/controllers/usuario.controller.js
+++ modulo06/DEV/Chatzap/frontend/js/controllers/usuario.controller.js
@@ -5,17 +5,21 @@ angular.module('app').controller('UsuariosController', function ($scope, usuario
 
   // Funções internas
   function create(usuario) {
-    usuarioService.create(usuario).then(function () {
+    usuarioService.create(usuario).then(function (response) {
+      usuario = response.data;
       list();
+      localStorage.setItem('userId', usuario.Id);
+      localStorage.setItem('userName', usuario.Nome);
+      localStorage.setItem('userPic', usuario.Foto);
     });
   };
 
   function entrarNaConversa () {
+    if(localStorage.getItem('userId') !== null) return;
     var userName = prompt("Digite seu Nome");
     var foto = prompt("Digite uma URL pública pra sua foto");
     var usuario = {Id: 0, Nome: userName, Foto: foto};
     create(usuario);
-    usuarioService.usuario = usuario;
   }
 
   function list() {
