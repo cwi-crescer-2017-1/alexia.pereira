a50389867b55e38b8b3728ec1980c4c2427add30
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-29 22:37:58 -0300

    feat(ProdutoRepositorio): adicionar Alterar, Obter e Excluir.

--- modulo06/DEV/aula04/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
+++ modulo06/DEV/aula04/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -8,9 +8,9 @@ namespace Demo1.Infraestrutura.Repositorios
     {
         string stringConexao = "Server=13.65.101.67;User Id=alexia.pereira;Password=123456;Database=aluno16db";
 
-        public void Criar (Produto produto)
+        public void Criar(Produto produto)
         {
-            
+
             using (var conexao = new SqlConnection(stringConexao))
             {
                 conexao.Open();
@@ -41,7 +41,7 @@ namespace Demo1.Infraestrutura.Repositorios
             }
         }
 
-        public List<Produto> Listar ()
+        public List<Produto> Listar()
         {
             var produtos = new List<Produto>();
 
@@ -52,7 +52,7 @@ namespace Demo1.Infraestrutura.Repositorios
                 {
                     comando.CommandText = "SELECT Id, Nome, Preco, Estoque FROM Produto";
                     var dataReader = comando.ExecuteReader();
-                    while(dataReader.Read())
+                    while (dataReader.Read())
                     {
                         Produto produto = new Produto();
                         produto.Id = (int)dataReader["Id"];
@@ -68,5 +68,79 @@ namespace Demo1.Infraestrutura.Repositorios
             return produtos;
         }
 
+        public void Alterar(Produto produto)
+        {
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                // Executa o INSERT
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        @"UPDATE PRODUTO SET Nome = @nome, Preco = @preco, Estoque = @estoque WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@nome", produto.Nome);
+                    comando.Parameters.AddWithValue("@preco", produto.Preco);
+                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+                    comando.Parameters.AddWithValue("@id", produto.Id);
+
+                    // Executa o comando e 
+                    // retorna somente a quantidade de linhas afetads
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                // Executa o INSERT
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "DELETE Produto WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    // Executa o comando e 
+                    // retorna somente a quantidade de linhas afetads
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public Produto Obter(int id)
+        {
+            Produto produto = null;
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        "SELECT Id, Nome, Preco, Estoque FROM Produto WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        produto = new Produto();
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+                        return produto;
+                    }
+                }
+            }
+
+            return produto;
+        }
     }
 }
