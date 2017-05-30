bf1b874c4ff61bd02a4871cd18a67e2e7158e618
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-29 22:38:53 -0300

    feat(PedidoRepositorio): adicionar repositorio de pedidos.

---
+++ modulo06/DEV/aula04/Demo1/Demo1.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,180 @@
+﻿using Demo1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    public class PedidoRepositorio : IPedidoRepositorio
+    {
+        string stringConexao = "Server=13.65.101.67;User Id=alexia.pereira;Password=123456;Database=aluno16db";
+
+        public void Alterar(Pedido pedido)
+        {
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                // Executa o INSERT
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        @"UPDATE Pedido SET NomeCliente = @nome WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@nome", pedido.NomeCliente);
+                    comando.Parameters.AddWithValue("@id", pedido.Id);
+
+                    // Executa o comando e 
+                    // retorna somente a quantidade de linhas afetads
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Criar(Pedido pedido)
+        {
+            // realizar o insert do Pedido
+            // obter o ultimo id do pedido (SELECT @@IDENTITY)
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "INSERT INTO Pedido (NomeCliente) VALUES (@nomeCliente)";
+                    comando.Parameters.AddWithValue("@nomeCliente", pedido.NomeCliente);
+
+                    comando.ExecuteNonQuery();
+                }
+                //Obtem o ID criado
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT @@IDENTITY";
+                    //Executa o comando e lê o primeiro comando de retorno
+                    var result = (decimal)comando.ExecuteScalar();
+                    pedido.Id = (int)result;
+                }
+            }
+
+            // para cada item do pedido, realizar o insert do ItemPedido
+            // obter o ultimo id do ItemPedido (SELECT @@IDENTITY)
+            foreach (var itemPedido in pedido.Itens)
+            {
+                using (var conexao = new SqlConnection(stringConexao))
+                {
+                    conexao.Open();
+                    using (var comando = conexao.CreateCommand())
+                    {
+                        comando.CommandText = @"INSERT INTO ItemPedido (PedidoId, ProdutoId, Quantidade) 
+                                                VALUES (@pedidoId, @produtoId, @quantidade)";
+                        comando.Parameters.AddWithValue("@pedidoId", pedido.Id);
+                        comando.Parameters.AddWithValue("@produtoId", itemPedido.ProdutoId);
+                        comando.Parameters.AddWithValue("@quantidade", itemPedido.Quantidade);
+
+                        comando.ExecuteNonQuery();
+                    }
+                    //Obtem o ID criado
+                    using (var comando = conexao.CreateCommand())
+                    {
+                        comando.CommandText = "SELECT @@IDENTITY";
+                        //Executa o comando e lê o primeiro comando de retorno
+                        var result = (decimal)comando.ExecuteScalar();
+                        itemPedido.Id = (int)result;
+                    }
+                }
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            var pedido = this.Obter(id);
+            foreach (var item in pedido.Itens)
+            {
+                using (var conexao = new SqlConnection(stringConexao))
+                {
+                    conexao.Open();
+
+                    using (var comando = conexao.CreateCommand())
+                    {
+                        comando.CommandText = "DELETE ItemPedido WHERE Id = @id";
+
+                        comando.Parameters.AddWithValue("@id", item.Id);
+
+                        comando.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "DELETE Pedido WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public IEnumerable<Pedido> Listar()
+        {
+            var pedidos = new List<Pedido>();
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT Id, NomeCliente FROM Pedido";
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        Pedido pedido = new Pedido();
+                        pedido.Id = (int)dataReader["Id"];
+                        pedido.NomeCliente = (string)dataReader["NomeCliente"];
+
+                        pedidos.Add(pedido);
+                    }
+                }
+            }
+            return pedidos;
+        }
+
+        public Pedido Obter(int id)
+        {
+            Pedido pedido = null;
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        "SELECT Id, NomeCliente FROM Produto WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        pedido = new Pedido();
+                        pedido.Id = (int)dataReader["Id"];
+                        pedido.NomeCliente = (string)dataReader["NomeCliente"];
+
+                        return pedido;
+                    }
+                }
+            }
+
+            return pedido;
+        }
+    }
+}
+
