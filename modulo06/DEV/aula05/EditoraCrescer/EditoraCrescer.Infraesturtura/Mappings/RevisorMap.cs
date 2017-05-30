fd1de54d877dadea4c2b3ab71e43e90dbb436dde
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-30 20:39:22 -0300

    feat(revisor-map): adicionar mapping de revisor.

---
+++ modulo06/DEV/aula05/EditoraCrescer/EditoraCrescer.Infraesturtura/Mappings/RevisorMap.cs
@@ -0,0 +1,21 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Mappings
+{
+    public class RevisorMap : EntityTypeConfiguration<Revisor>
+    {
+
+        public RevisorMap()
+        {
+            ToTable("Revisores");
+            Property(p => p.Nome).HasMaxLength(300);
+        }
+
+    }
+}
