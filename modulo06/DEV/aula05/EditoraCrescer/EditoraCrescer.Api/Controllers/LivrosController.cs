65b97a12388a7f4878b8c572e0e2a18e00889b78
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-31 17:56:14 -0300

    feat(rotas): adicionar rotas ao controller dos livros.

--- modulo06/DEV/aula05/EditoraCrescer/EditoraCrescer.Api/Controllers/LivrosController.cs
+++ modulo06/DEV/aula05/EditoraCrescer/EditoraCrescer.Api/Controllers/LivrosController.cs
@@ -14,48 +14,50 @@ namespace EditoraCrescer.Api.Controllers
     public class LivrosController : ApiController
     {
         private LivroRepositorio repositorio = new LivroRepositorio();
-        
-        public IHttpActionResult Get ()
+
+        [HttpGet]
+        public IHttpActionResult Get()
         {
             var livros = repositorio.Obter();
             return Ok(livros);
         }
 
-        [Route("{isbn:int}")]
+        [Route("{isbn:int}"), HttpGet]
         public IHttpActionResult Get(int isbn)
         {
             var livro = repositorio.Obter(isbn);
             return Ok(livro);
         }
 
-        [Route("{genero}")]
+        [Route("{genero}"), HttpGet]
         public IHttpActionResult Get(string genero)
         {
             var livro = repositorio.Obter(genero);
             return Ok(livro);
         }
 
+        [HttpPost, Route]
         public IHttpActionResult Post(Livro livro)
         {
             repositorio.Cadastrar(livro);
             return Ok(livro);
         }
 
-        [Route("{isbn:int}")]
-        public IHttpActionResult Delete (int isbn)
+        [Route("{isbn:int}"), HttpDelete]
+        public IHttpActionResult Delete(int isbn)
         {
             repositorio.Remover(isbn);
             return Ok();
         }
 
-        [Route("{isbn:int}")]
-        public IHttpActionResult Put (int isbn, Livro livro)
+        [Route("{isbn:int}"), HttpPut]
+        public IHttpActionResult Put(int isbn, Livro livro)
         {
             if (isbn != livro.Isbn)
                 return BadRequest();
             return Ok(repositorio.Atualizar(livro));
         }
-        
+
 
         protected override void Dispose(bool disposing)
         {
