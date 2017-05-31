78673eb5f08c5bc0978f47405855f37ae29a99b8
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-05-30 22:35:45 -0300

    feat(Revisores-Get): adicionar método Get aos revisores.

---
+++ modulo06/DEV/aula05/EditoraCrescer/EditoraCrescer.Api/Controllers/RevisoresController.cs
@@ -0,0 +1,21 @@
+﻿using EditoraCrescer.Infraesturtura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    public class RevisoresController : ApiController
+    {
+        private RevisoresRepositorio repositorio = new RevisoresRepositorio();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(repositorio.Obter());
+        }
+        
+    }
+}
---
+++ modulo06/DEV/aula05/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/RevisoresRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Repositorios
+{
+    public class RevisoresRepositorio
+    {
+
+        Contexto contexto = new Contexto();
+
+        public List<Revisor> Obter()
+        {
+            return contexto.Revisores.ToList();
+        }
+
+    }
+}
