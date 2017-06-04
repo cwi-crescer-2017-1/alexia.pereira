3059817020830fb7c0f8d03313b4b81f889ea9d4
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-03 22:48:21 -0300

    feat(visualizar livro): adicionar métodos dos adms para os livros.

--- modulo06/DEV/aula05/EditoraCrescer/frontend/livros/livro.visualizar.controller.js
+++ modulo06/DEV/aula05/EditoraCrescer/frontend/livros/livro.visualizar.controller.js
@@ -1,14 +1,42 @@
 angular.module('app')
-  .controller('LivroVisualizarController', function ($scope, $routeParams, livrosService) {
+  .controller('LivroVisualizarController', function ($scope, $routeParams, livrosService, autoresService, authService) {
 
     $scope.livro = {};
+    $scope.permitirEdicao = permitirEdicao;
+    $scope.edit = livrosService.edit;
+    $scope.usuarioLogado = usuarioLogado;
+    $scope.atualizarLivro = atualizarLivro;
 
     buscarLivro($routeParams.isbn);
+    buscarAutores();
 
     function buscarLivro(isbn) {
     livrosService.procurarPorIsbn(isbn).then(function (response) {
         $scope.livro = response.data.dados;
       })
     };
+
+    function buscarAutores () {
+      autoresService.listar().then(function (response) {
+        console.log(response.data);
+        $scope.autores = response.data;
+      })
+    };
+
+    function atualizarLivro(livro) {
+      livrosService.atualizar(livro).then(function(response) {
+        $scope.livro = response.data.dados;
+        $scope.edit = false;
+        livrosService.edit = false;
+      });
+    }
+
+    function permitirEdicao() {
+      $scope.edit = true;
+    }
+
+    function usuarioLogado() {
+      return authService.isAutenticado();
+    }
 
   });
