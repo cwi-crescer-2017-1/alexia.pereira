bb63b5461c3e33936927e4d7ca968a8a6e08051c
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-03 22:47:08 -0300

    feat(rotas): adicionar rotas.

--- modulo06/DEV/aula05/EditoraCrescer/frontend/app.route.js
+++ modulo06/DEV/aula05/EditoraCrescer/frontend/app.route.js
@@ -8,5 +8,19 @@ angular.module('app').config(function ($routeProvider) {
     controller: 'LivroVisualizarController',
     templateUrl: '/livros/livro.visualizar.html'
   })
+  .when('/login', {
+    controller: 'LoginController',
+    templateUrl: 'login/login.html'
+  })
+  .when('/adminstrativo', {
+      controller: 'AdministrativoController',
+      templateUrl: 'administrativo/administrativo.html',
+      resolve: {
+        // define que para acessar esta página deve ser um usuário autenticado (mas não restringe o tipo de permissão)
+        autenticado: function (authService) {
+          return authService.isAutenticadoPromise();
+        }
+      }
+    })
   .otherwise({redirectTo: '/livros'});
 });
