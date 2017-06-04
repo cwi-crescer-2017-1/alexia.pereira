79cab87bb08da82c13449c15ddf2976fb44afcd1
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-03 22:48:33 -0300

    feat(login): adicionar login.

---
+++ modulo06/DEV/aula05/EditoraCrescer/frontend/login/login.controller.js
@@ -0,0 +1,18 @@
+angular.module('app').controller('LoginController', function ($location, $scope, authService) {
+
+$scope.login = login;
+
+function login (usuario) {
+  authService.login(usuario)
+      .then(
+        function (response) {
+          console.log(response);
+          alert('Login com sucesso!');
+
+        },
+        function (response) {
+          console.log(response);
+          alert('Erro no Login!');
+        });
+      }
+});
