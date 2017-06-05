58744689f8715c22b72be46dc5a6bcba7eba4af9
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-05 20:46:56 -0300

    test(Funcionario): adicionar testes unitários para validar funcionário.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Dominio.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LocadoraCrescer.Dominio.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LocadoraCrescer.Dominio.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("723c3fdd-0068-44f7-bcc1-75930965b644")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Dominio.UnitTest/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dominio.Entidades;
+using System.Linq;
+
+namespace LocadoraCrescer.Dominio.UnitTest
+{
+
+    [TestClass]
+    public class FuncionarioUnitTest
+    {
+        [TestMethod]
+        public void Deve_Criar_Entidade_Funcionario_Valida()
+        {
+            var funcionario = new Funcionario("Giovani", "giovani@cwi.com.br", "123456");
+            Assert.IsTrue(funcionario.Validar());
+            Assert.IsFalse(funcionario.Mensagens.Any());
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Funcionario_Sem_Nome()
+        {
+            var funcionario = new Funcionario("", "giovani@cwi.com.br", "123456");
+            Assert.IsFalse(funcionario.Validar());
+            Assert.IsTrue(funcionario.Mensagens.Any());
+            Assert.IsTrue(funcionario.Mensagens[0] == "Nome é inválido.");
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Funcionario_Sem_Email()
+        {
+            var funcionario = new Funcionario("Giovani", "", "123456");
+            Assert.IsFalse(funcionario.Validar());
+            Assert.IsTrue(funcionario.Mensagens.Any());
+            Assert.IsTrue(funcionario.Mensagens[0] == "Email é inválido.");
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Funcionario_Sem_Senha()
+        {
+            var funcionario = new Funcionario("Giovani", "giovani@cwi.com.br", "");
+            Assert.IsFalse(funcionario.Validar());
+            Assert.IsTrue(funcionario.Mensagens.Any());
+            Assert.IsTrue(funcionario.Mensagens[0] == "Senha é inválido.");
+        }
+
+        [TestMethod]
+        public void Deve_Validar_Senha_Funcionario_Correta()
+        {
+            var funcionario = new Funcionario("Giovani", "giovani@cwi.com.br", "123456");
+            Assert.IsTrue(funcionario.ValidarSenha("123456"));
+        }
+
+        [TestMethod]
+        public void Deve_Criptografar_Senha_Funcionario()
+        {
+            var funcionario = new Funcionario("Giovani", "giovani@cwi.com.br", "123456");
+            Assert.IsTrue(funcionario.Senha != "123456");
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Funcionario_Com_Permissoes_Operador()
+        {
+            var funcionario = new Funcionario("Giovani", "giovani@cwi.com.br", "123456");
+            Assert.IsTrue(funcionario.Permissao == "Operador");
+        }
+        
+    }
+}
