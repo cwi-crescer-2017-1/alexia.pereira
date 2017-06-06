6d96f8e7834ede80b1871b6b27fbe43fa4d30e5d
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-06 16:55:17 -0300

    feat(locacaoOpcional): adicionar entidade que relaciona locação com opcionais.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/Dominio/Entidades/LocacaoOpcional.cs
@@ -0,0 +1,17 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Dominio.Entidades
+{
+    public class LocacaoOpcional
+    {
+        public int Id { get; set; }
+        public Locacao Locacao { get; set; }
+        public Opcional Opcional { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
