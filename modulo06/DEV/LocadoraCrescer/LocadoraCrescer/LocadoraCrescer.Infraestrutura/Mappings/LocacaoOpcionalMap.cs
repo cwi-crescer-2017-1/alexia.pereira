318ca0ccd509c6913d1881ef65ba7e732372fa0a
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-06 17:01:17 -0300

    feat(map-LocacaoOperacional): adicionar mapping de LocacaoOperacional.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Infraestrutura/Mappings/LocacaoOpcionalMap.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Infraestrutura.Mappings
+{
+    public class LocacaoOpcionalMap : EntityTypeConfiguration<LocacaoOpcional>
+    {
+        public LocacaoOpcionalMap()
+        {
+            ToTable("LocacaoOpcional");
+            HasRequired(x => x.Locacao).WithMany().Map(x => x.MapKey("IdLocacao"));
+            HasRequired(x => x.Opcional).WithMany().Map(x => x.MapKey("IdOpcional"));
+        }
+    }
+}
