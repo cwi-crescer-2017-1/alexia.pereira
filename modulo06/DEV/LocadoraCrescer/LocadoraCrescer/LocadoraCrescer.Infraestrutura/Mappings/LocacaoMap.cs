73fe58499358023b2048c9a0cb943eb5cce5d6e3
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-06 16:58:45 -0300

    feat(map-locacao): adicionar mapping de locacao.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Infraestrutura/Mappings/LocacaoMap.cs
@@ -0,0 +1,24 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Infraestrutura.Mappings
+{
+    public class LocacaoMap : EntityTypeConfiguration<Locacao>
+    {
+        public LocacaoMap()
+        {
+            ToTable("Locacao");
+            HasRequired(x => x.Veiculo).WithMany().Map(x => x.MapKey("IdVeiculo"));
+
+            HasRequired(x => x.Cliente).WithMany().Map(x => x.MapKey("IdCliente"));
+
+            HasOptional(x => x.Pacote).WithMany().Map(x => x.MapKey("IdPacote"));
+
+        }
+    }
+}
