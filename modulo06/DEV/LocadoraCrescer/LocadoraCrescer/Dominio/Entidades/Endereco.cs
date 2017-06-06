79b682d5cc693fa71bf9a35801c32be09479a631
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-05 21:30:11 -0300

    test(Endereco): adicionar testes para validação de endereço.

--- modulo06/DEV/LocadoraCrescer/LocadoraCrescer/Dominio/Entidades/Endereco.cs
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/Dominio/Entidades/Endereco.cs
@@ -1,12 +1,47 @@
-﻿namespace LocadoraCrescer.Dominio.Entidades
+﻿using System;
+using Dominio.Entidades;
+
+namespace LocadoraCrescer.Dominio.Entidades
 {
-    public class Endereco
+    public class Endereco : EntidadeBasica
     {
         public int Id { get; set; }
         public string Cidade { get; set; }
         public string Rua { get; set; }
-        public int Numero { get; set; }
+        public string Numero { get; set; }
         public string UF { get; set; }
         public string CEP { get; set; }
+
+        public Endereco(string cidade, string rua, string numero, string uf, string cep)
+        {
+            Id = 0;
+            Cidade = cidade;
+            Rua = rua;
+            Numero = numero;
+            UF = uf;
+            CEP = cep;
+        }
+
+        public override bool Validar()
+        {
+            Mensagens.Clear();
+
+            if (string.IsNullOrWhiteSpace(Cidade))
+                Mensagens.Add("Cidade é inválida.");
+
+            if (string.IsNullOrWhiteSpace(Rua))
+                Mensagens.Add("Rua é inválida.");
+
+            if (string.IsNullOrWhiteSpace(Numero))
+                Mensagens.Add("Número inválido.");
+
+            if (UF.Length > 2 || string.IsNullOrWhiteSpace(UF))
+                Mensagens.Add("Unidade Federatica inválida.");
+
+            if (string.IsNullOrWhiteSpace(CEP))
+                Mensagens.Add("CEP inválido.");
+
+            return Mensagens.Count == 0;
+        }
     }
 }
---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Dominio.UnitTest/EnderecoUnitTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocadoraCrescer.Dominio.Entidades;
+using System.Linq;
+
+namespace LocadoraCrescer.Dominio.UnitTest
+{
+    [TestClass]
+    public class EnderecoUnitTest
+    {
+        [TestMethod]
+        public void Deve_Criar_Entidade_Endereco_Valida()
+        {
+            var endereco = new Endereco("Sao Leopoldo", "Rua Teste", "123", "RS", "90909090");
+            Assert.IsTrue(endereco.Validar());
+            Assert.IsFalse(endereco.Mensagens.Any());
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Endereco_Sem_Cidade()
+        {
+            var endereco = new Endereco("", "Rua Teste", "123", "RS", "90909090");
+            Assert.IsFalse(endereco.Validar());
+            Assert.IsTrue(endereco.Mensagens.Any());
+            Assert.IsTrue(endereco.Mensagens[0] == "Cidade é inválida.");
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Endereco_Sem_Rua()
+        {
+            var endereco = new Endereco("Sao Leopoldo", "", "123", "RS", "90909090");
+            Assert.IsFalse(endereco.Validar());
+            Assert.IsTrue(endereco.Mensagens.Any());
+            Assert.IsTrue(endereco.Mensagens[0] == "Rua é inválida.");
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Endereco_Sem_Numero()
+        {
+            var endereco = new Endereco("Sao Leopoldo", "Rua Teste", "", "RS", "90909090");
+            Assert.IsFalse(endereco.Validar());
+            Assert.IsTrue(endereco.Mensagens.Any());
+            Assert.IsTrue(endereco.Mensagens[0] == "Número inválido.");
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Endereco_Sem_UF()
+        {
+            var endereco = new Endereco("Sao Leopoldo", "Rua Teste", "123", "", "90909090");
+            Assert.IsFalse(endereco.Validar());
+            Assert.IsTrue(endereco.Mensagens.Any());
+            Assert.IsTrue(endereco.Mensagens[0] == "Unidade Federatica inválida.");
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Endereco_Com_UF_Maior_Que_Dois()
+        {
+            var endereco = new Endereco("Sao Leopoldo", "Rua Teste", "123", "RSRSRS", "90909090");
+            Assert.IsFalse(endereco.Validar());
+            Assert.IsTrue(endereco.Mensagens.Any());
+            Assert.IsTrue(endereco.Mensagens[0] == "Unidade Federatica inválida.");
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Validar_Entidade_Endereco_Sem_CEP()
+        {
+            var endereco = new Endereco("Sao Leopoldo", "Rua Teste", "123", "RS", "");
+            Assert.IsFalse(endereco.Validar());
+            Assert.IsTrue(endereco.Mensagens.Any());
+            Assert.IsTrue(endereco.Mensagens[0] == "CEP inválido.");
+        }
+
+    }
+}
