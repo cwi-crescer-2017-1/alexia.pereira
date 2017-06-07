5b37414c1b8fce71ceb3941004bc27660ebeb109
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-07 16:08:17 -0300

    feat(frontend-login): adicionar tela e controller de login.

---
+++ modulo06/DEV/LocadoraCrescer/Frontend/login/login.controller.js
@@ -0,0 +1,19 @@
+angular.module('app').controller('LoginController', function ($location, $scope, authService) {
+
+$scope.login = login;
+
+function login (usuario) {
+  debugger;
+  authService.login(usuario)
+      .then(
+        function (response) {
+          console.log(response);
+          alert('Login com sucesso!');
+
+        },
+        function (response) {
+          console.log(response);
+          alert('Erro no Login!');
+        });
+      }
+});
