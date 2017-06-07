bb9eec690a41811e74cefb5d7563fb1b35a66973
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-07 17:40:05 -0300

    feat(frontend-veiculoService): adicionar service de veiculos.

---
+++ modulo06/DEV/LocadoraCrescer/Frontend/veiculo/veiculo.service.js
@@ -0,0 +1,12 @@
+angular.module('app').factory('veiculoService', function ($http) {
+
+  let urlBase = 'http://localhost:64375/api/veiculos/';
+
+  function getTodosOsVeiculos() {
+    return $http.get(urlBase);
+  };
+
+  return {
+    listar: getTodosOsVeiculos,
+  };
+});
