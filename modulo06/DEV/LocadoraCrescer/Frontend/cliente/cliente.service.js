607c4b8f292f2adf157c439553f4bf025913a32c
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-07 16:59:41 -0300

    feat(frontend-clienteService): adicionar service de cliente.

---
+++ modulo06/DEV/LocadoraCrescer/Frontend/cliente/cliente.service.js
@@ -0,0 +1,27 @@
+angular.module('app').factory('clienteService', function ($http) {
+
+  let urlBase = 'http://localhost:64375/api/clientes/';
+
+  function getTodosOsClientes() {
+    return $http.get(urlBase + 'todos');
+  };
+
+  function getUsuarioPorCpf(cpf) {
+    return $http.get(urlBase + cpf);
+  };
+
+  function atualizar(cliente) {
+    return $http.put(urlBase + 'atualizar', cliente);
+  };
+
+  function criar (cliente) {
+    return $http.post(urlBase + 'registrar', cliente);
+  }
+
+  return {
+    listar: getTodosOsClientes,
+    procurarUsuarioPorCpf: getUsuarioPorCpf,
+    atualizar: atualizar,
+    criar: criar
+  };
+});
