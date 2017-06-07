8009a16ce6891b1accf4afbbb7d59c884422db11
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-07 14:28:30 -0300

    feat(locacaoOpcionalModel): adicionar model de LocacaOpcional.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Api/Models/LocacaoOpcional.cs
@@ -0,0 +1,22 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocadoraCrescer.Api.Models
+{
+    public class LocacaoOpcional
+    {
+        public int Id { get; set; }
+        public Veiculo Veiculo { get; set; }
+        public Cliente Cliente { get; set; }
+        public Pacote Pacote { get; set; }
+        public DateTime DataLocacao { get; set; }
+        public DateTime DataEntregaPrevista { get; set; }
+        public DateTime? DataEntregaReal { get; set; }
+        public decimal ValorLocacao { get; set; }
+        public decimal ValorDesconto { get; set; }
+        public List<LocacaoOpcional> LocacaoOpcionais { get; set; }
+    }
+}
