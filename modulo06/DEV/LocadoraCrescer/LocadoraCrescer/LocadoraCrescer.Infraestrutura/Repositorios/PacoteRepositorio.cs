d587e23afcd70f06dd73bd0c05d94147105e1b7e
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-06 21:43:52 -0300

    feat(backend-pacoteRepositorio): adicionar repositório de pacotes.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Infraestrutura/Repositorios/PacoteRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Infraestrutura.Repositorios
+{
+    public class PacotesRepositorio : IDisposable
+    {
+        Contexto contexto;
+
+        public PacotesRepositorio()
+        {
+            contexto = new Contexto();
+        }
+
+        public List<Pacote> Obter()
+        {
+            return contexto.Pacotes.ToList();
+        }
+
+        
+        public Pacote Obter(int id)
+        {
+            return contexto.Pacotes.FirstOrDefault(p => p.Id == id);
+        }
+        
+        public bool PacoteExiste(int id)
+        {
+            return contexto.Pacotes.Count(l => l.Id == id) > 0;
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+    }
+}
