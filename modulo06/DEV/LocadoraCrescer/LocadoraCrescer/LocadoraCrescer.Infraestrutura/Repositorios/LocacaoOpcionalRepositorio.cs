1236bd6c20fd9745fb47b73c9fbfebfd383a403f
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-06 21:44:51 -0300

    feat(backend-locacaoOpcionalRepositorio): adicionar repositorio de locações opcionais.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Infraestrutura/Repositorios/LocacaoOpcionalRepositorio.cs
@@ -0,0 +1,67 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Infraestrutura.Repositorios
+{
+    public class LocacoesOpcionaisRepositorio : IDisposable
+    {
+        Contexto contexto;
+
+        public LocacoesOpcionaisRepositorio()
+        {
+            contexto = new Contexto();
+        }
+
+        public List<LocacaoOpcional> Obter()
+        {
+            return contexto.LocacoesOpcionais.ToList();
+        }
+
+        public void Cadastrar(LocacaoOpcional locacaoOpcional)
+        {
+            contexto.LocacoesOpcionais.Add(locacaoOpcional);
+            contexto.SaveChanges();
+        }
+
+        public LocacaoOpcional Obter(int id)
+        {
+            return contexto.LocacoesOpcionais.FirstOrDefault(l => l.Id == id);
+        }
+
+        public List<LocacaoOpcional> ObterPorLocacao(int idLocacao)
+        {
+            return contexto.LocacoesOpcionais
+                .Where(lo => lo.Locacao.Id == idLocacao)
+                .ToList();
+        }
+
+        public void Remover(int id)
+        {
+            contexto.LocacoesOpcionais.Remove(Obter(id));
+            contexto.SaveChanges();
+        }
+
+        public LocacaoOpcional Atualizar(LocacaoOpcional locacaoOpcional)
+        {
+            contexto.Entry(locacaoOpcional).State = EntityState.Modified;
+            contexto.SaveChanges();
+            return locacaoOpcional;
+        }
+
+        public bool LocacaoOpcionalExiste(int id)
+        {
+            return contexto.LocacoesOpcionais.Count(l => l.Id == id) > 0;
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+    }
+}
