9add7cf0eb4f952877039fb73bea169c3d02973f
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-07 17:39:37 -0300

    feat(backend-veiculoController): adicionar controller de veiculo.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Api/Controllers/VeiculosController.cs
@@ -0,0 +1,23 @@
+﻿using LocadoraCrescer.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocadoraCrescer.Api.Controllers
+{
+    [BasicAuthorization]
+    public class VeiculosController : ControllerBasica
+    {
+
+        VeiculoRepositorio repositorio = new VeiculoRepositorio();
+
+        [HttpGet]
+        public HttpResponseMessage Obter()
+        {
+            return ResponderOK(new { dados = repositorio.Obter() });
+        }
+    }
+}
