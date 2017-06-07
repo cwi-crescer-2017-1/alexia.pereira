56dbe47490a6a35238c42c4455aa69ff60021f28
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-06 21:08:49 -0300

    feat(backend-veiculoRepositorio): adicionar repositorio de veiculos.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Infraestrutura/Repositorios/VeiculoRepositorio.cs
@@ -0,0 +1,50 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Infraestrutura.Repositorios
+{
+    public class VeiculoRepositorio : IDisposable
+    {
+        Contexto contexto;
+
+        public VeiculoRepositorio()
+        {
+            contexto = new Contexto();
+        }
+
+        public void Criar(Veiculo veiculo)
+        {
+            contexto.Veiculos.Add(veiculo);
+            contexto.SaveChanges();
+        }
+
+        public List<Veiculo> Obter()
+        {
+            return contexto.Veiculos.ToList();
+        }
+
+        public Veiculo Obter(int id)
+        {
+            return contexto.Veiculos.FirstOrDefault(v => v.Id == id);
+        }
+
+        public Veiculo AtualizarEstoque(Veiculo veiculo)
+        {
+            contexto.Entry(veiculo).State = EntityState.Modified;
+            veiculo.Quantidade--;
+            contexto.SaveChanges();
+            return veiculo;
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+    }
+}
