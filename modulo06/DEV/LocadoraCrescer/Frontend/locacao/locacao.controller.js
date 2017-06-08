23ca12d795def77c009b11b3bf924a1f72ce2ab2
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-07 23:54:17 -0300

    feat(frontend-locacao): adicionar campo no escopo que verifica se há um cliente válido.

--- modulo06/DEV/LocadoraCrescer/Frontend/locacao/locacao.controller.js
+++ modulo06/DEV/LocadoraCrescer/Frontend/locacao/locacao.controller.js
@@ -12,17 +12,18 @@ angular.module('app').controller('LocacaoController', function ($location, $scop
   }
 
   function buscarClientePorCpf(cpf) {
-    $scope.clienteCadastrado = true;
     clienteService.buscarClientePorCpf(cpf).then(function (response) {
       $scope.locacao.cliente = response.data.dados;
       if ($scope.locacao.cliente == null) {
         $scope.clienteCadastrado = false;
         alert("Não há nenhum cliente com esse cpf")
         return;
+      } else {
+        $scope.clienteCadastrado = true;
+        alert('Cliente encontrado! Nome do Cliente: ' + $scope.locacao.cliente.Nome);
       }
-      alert('Cliente encontrado! Nome do Cliente: ' + $scope.locacao.cliente.Nome);
-      })
-    };
+    })
+  };
 
 
   function cadastrarCliente () {
