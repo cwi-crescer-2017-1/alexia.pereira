951382d3bc3d18307a410cd06bae9d61cd36b334
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-08 13:38:55 -0300

    feat(clienteModel): adicionar model de cliente.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Api/Models/ClienteModel.cs
@@ -0,0 +1,18 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocadoraCrescer.Api.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Genero Genero { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
