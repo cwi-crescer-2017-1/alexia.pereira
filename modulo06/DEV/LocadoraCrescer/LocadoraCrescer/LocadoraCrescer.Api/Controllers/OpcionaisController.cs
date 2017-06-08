685ad02e84f2e1404b781a2b90efad8d44d2401e
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-08 17:49:40 -0300

    feat(backend-controllers): adicionar controllers de opcionais e pacotes.

---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Api/Controllers/OpcionaisController.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraCrescer.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocadoraCrescer.Api.Controllers
+{
+    public class OpcionaisController : ControllerBasica
+    {
+        OpcionaisRepositorio repositorio = new OpcionaisRepositorio();
+
+        public HttpResponseMessage Get ()
+        {
+            return ResponderOK(repositorio.Obter());
+        }
+    }
+}
---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Api/Controllers/PacotesController.cs
@@ -0,0 +1,21 @@
+﻿using LocadoraCrescer.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocadoraCrescer.Api.Controllers
+{
+    [BasicAuthorization]
+    public class PacotesController : ControllerBasica
+    {
+        PacotesRepositorio repositorio = new PacotesRepositorio();
+
+        public HttpResponseMessage Get ()
+        {
+            return ResponderOK(repositorio.Obter());
+        }
+    }
+}
