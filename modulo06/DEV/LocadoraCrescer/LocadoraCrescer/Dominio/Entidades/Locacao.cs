daf54b5cf93f638dcd1de86b31c5bca0dbc06be1
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-08 23:26:55 -0300

    feat(locacao-test): adicionar testes básicos para a locacao.

--- modulo06/DEV/LocadoraCrescer/LocadoraCrescer/Dominio/Entidades/Locacao.cs
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/Dominio/Entidades/Locacao.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LocadoraCrescer.Dominio.Entidades
 {
-    public class Locacao
+    public class Locacao : EntidadeBasica
     {
         public int Id { get; set; }
         public Veiculo Veiculo { get; set; }
@@ -25,15 +25,35 @@ namespace LocadoraCrescer.Dominio.Entidades
         }
 
         public Locacao(Veiculo veiculo, Cliente cliente, Pacote pacote,
-            DateTime dataEntregaPrevista, decimal valorLocacao, List<LocacaoOpcional> locacaoOpcionais)
+            DateTime dataEntregaPrevista, decimal valorLocacao)
         {
             Veiculo = veiculo;
             Cliente = cliente;
             Pacote = pacote;
             DataEntregaPrevista = dataEntregaPrevista;
             ValorLocacao = valorLocacao;
-            LocacaoOpcionais = locacaoOpcionais;
             DataLocacao = DateTime.Now;
+            LocacaoOpcionais = new List<LocacaoOpcional>();
+        }
+
+        public bool ValidarVeiculoEOpcional()
+        {
+            foreach (var locacaoOpcional in LocacaoOpcionais)
+            {
+                if (locacaoOpcional.Opcional.Descricao == "Reboque" && Veiculo.Nome == "Mobi")
+                    Mensagens.Add("Mobi não pode ter Reboque.");
+                if (locacaoOpcional.Opcional.Descricao == "Rack" && Veiculo.Nome == "Hilux")
+                    Mensagens.Add("Hilux não pode ter hack.");
+                if (locacaoOpcional.Opcional.Descricao == "Cabo Bateria" && Veiculo.Nome != "Kombi")
+                    Mensagens.Add("Apenas Kombi pode ter Cabo Bateria.");
+            }
+
+            return Mensagens.Count == 0;
+        }
+
+        public override bool Validar()
+        {
+            return ValidarVeiculoEOpcional();
         }
     }
 }
---
+++ modulo06/DEV/LocadoraCrescer/LocadoraCrescer/LocadoraCrescer.Dominio.UnitTest/LocacaoUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocadoraCrescer.Dominio.Entidades;
+using System.Linq;
+
+namespace LocadoraCrescer.Dominio.UnitTest
+{
+    [TestClass]
+    public class LocacaoUnitTest
+    {
+        [TestMethod]
+        public void Deve_Criar_Locacao_Kombi_Cabo_Bateria()
+        {
+            var cliente = new Cliente("Teste", "70707070707", DateTime.Now, Genero.MASCULINO);
+            var veiculo = new Veiculo() { Nome = "Kombi" };
+            var locacao = new Locacao(veiculo, cliente, null, DateTime.Now, 1200);
+            locacao.LocacaoOpcionais.Add
+                (new LocacaoOpcional(locacao, new Opcional()
+                { Descricao = "Cabo Bateria"}));
+            Assert.IsTrue(locacao.Validar());
+            Assert.IsFalse(locacao.Mensagens.Any());
+        }
+
+        [TestMethod]
+        public void Nao_Deve_Criar_Locacao_Mobi_Cabo_Bateria()
+        {
+            var cliente = new Cliente("Teste", "70707070707", DateTime.Now, Genero.MASCULINO);
+            var veiculo = new Veiculo() { Nome = "Mobi" };
+            var locacao = new Locacao(veiculo, cliente, null, DateTime.Now, 1200);
+            locacao.LocacaoOpcionais.Add
+                (new LocacaoOpcional(locacao, new Opcional()
+                { Descricao = "Cabo Bateria" }));
+            Assert.IsTrue(!locacao.Validar());
+            Assert.IsTrue(locacao.Mensagens.Any());
+            Assert.IsTrue(locacao.Mensagens[0] == "Apenas Kombi pode ter Cabo Bateria.");
+        }
+    }
+}
