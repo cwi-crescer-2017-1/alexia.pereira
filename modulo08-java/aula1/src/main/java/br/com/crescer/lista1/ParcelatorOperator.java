c48f704609c60d6bbe80bba74df7aec9f95655f5
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-21 15:35:41 -0300

    feat(LinkedHashMap): adicionar LinkedHashMap no exercicio.

--- modulo08-java/aula1/src/main/java/br/com/crescer/lista1/ParcelatorOperator.java
+++ modulo08-java/aula1/src/main/java/br/com/crescer/lista1/ParcelatorOperator.java
@@ -6,8 +6,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class ParcelatorOperator implements Parcelator {
 
@@ -15,7 +15,7 @@ public class ParcelatorOperator implements Parcelator {
     public Map<String, BigDecimal> calcular(BigDecimal valorParcelar,
             int numeroParcelas, double taxaJuros, Date dataPrimeiroVencimento) {
 
-        Map<String, BigDecimal> conta = new TreeMap<String, BigDecimal>(Collections.reverseOrder());
+        Map<String, BigDecimal> conta = new LinkedHashMap<String, BigDecimal>();
 
         taxaJuros = taxaJuros / 100;
         Calendar c = Calendar.getInstance();
