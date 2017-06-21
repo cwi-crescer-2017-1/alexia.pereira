bc0c3e02011e2bda4317a18ba98c6d0cc271d1f3
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-21 01:11:35 -0300

    feat(main): adicionar coisas relevantes a classe main.

--- modulo08-java/aula1/src/main/java/br/com/crescer/lista1/Main.java
+++ modulo08-java/aula1/src/main/java/br/com/crescer/lista1/Main.java
@@ -3,13 +3,72 @@ package br.com.crescer.lista1;
  *
  * @author alexiapereira
  */
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String args[]) {
-		StringOperator stringOperator = new StringOperator();
-		System.out.println(stringOperator.isEmpty(null));
-		System.out.println(stringOperator.inverter("ovo ata banana"));
-		System.out.println(stringOperator.contaVogais("a ana tava lá"));
-		System.out.println(stringOperator.isPalindromo("A sogra má e amargosa"));
-	}
-}
+		try (final Scanner t = new Scanner(System.in)) {
+			
+			StringOperator stringOperator = new StringOperator();
+			
+			System.out.println("STRING UTILS:");
+			
+			System.out.println("Resultado do isEmpty com ''");
+			System.out.println(stringOperator.isEmpty(""));
+			System.out.println("-------------------------");
+			
+			System.out.println("Resultado do inverter com a frase: 'hello world'");
+			System.out.println(stringOperator.inverter("hello world"));
+			System.out.println("-------------------------");
+			
+			System.out.println("Resultado do contaVogais com a frase: 'a ana estava l�'");
+			System.out.println(stringOperator.contaVogais("a ana estava l�"));
+			System.out.println("-------------------------");
+			
+			System.out.println("Resultado do isPalindromo com a frase: 'A sogra m� e amargosa'");
+			System.out.println(stringOperator
+					.isPalindromo("A sogra m� e amargosa") + "\n");
+			System.out.println("-------------------------------------------------\n");
+			
+			
+			CalendarOperator calendarO = new CalendarOperator();
+			
+			System.out.println("CALENDAR UTILS:");
+			
+			System.out.println("Dia da Semana: ");
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			System.out.println("Digite a data: ");
+			Date date = sdf.parse(t.next());
+			System.out.println(calendarO.diaSemana(date));
+			System.out.println("-------------------------");
+			
+			System.out.println("Tempo Decorrido: ");
+			sdf = new SimpleDateFormat("dd/MM/yyyy");
+			System.out.println("Digite a data");
+			date = sdf.parse(t.next());
+			System.out.println(calendarO.tempoDecorrido(date) + "\n");
+			System.out.println("-------------------------------------------------\n");
+			
+			System.out.println("PARCELATOR: ");
+			System.out.println("Resultado do Parcelator com os dados da gist");
+			ParcelatorOperator po = new ParcelatorOperator();
+			sdf = new SimpleDateFormat("dd/MM/yyyy");
+			date = sdf.parse("30/06/2016");
+			Map<String, BigDecimal> conta = po.calcular(new BigDecimal(1000),
+					10, 0.1, date);
+			for (Entry<String, BigDecimal> pair : conta.entrySet()) {
+				System.out.println(pair.getKey() + " - " + pair.getValue());
+			}
+			
+		} catch (Exception e) {
 
+		}
+	}
+
+}
