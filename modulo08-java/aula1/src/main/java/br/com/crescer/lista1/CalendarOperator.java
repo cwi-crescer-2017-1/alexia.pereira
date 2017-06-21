c4d0b1baad5e7d624e355432c06c3353a9826c2c
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-21 13:57:28 -0300

    fix(tempoDecorrido): corrigir método de tempo decorrido.

--- modulo08-java/aula1/src/main/java/br/com/crescer/lista1/CalendarOperator.java
+++ modulo08-java/aula1/src/main/java/br/com/crescer/lista1/CalendarOperator.java
@@ -1,49 +1,51 @@
 package br.com.crescer.lista1;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.DayOfWeek;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
-public class CalendarOperator implements CalendarUtils{
+public class CalendarOperator implements CalendarUtils {
 
-	public DiaSemana diaSemana(Date date) {
-		Calendar c = Calendar.getInstance();
+    public DiaSemana diaSemana(Date date) {
+        Calendar c = Calendar.getInstance();
         c.setTime(date);
         int diaDaSemana = c.get(Calendar.DAY_OF_WEEK);
-		return descobrirDiaDaSemanaPorNumero(diaDaSemana);
-	}
+        return descobrirDiaDaSemanaPorNumero(diaDaSemana);
+    }
 
-	public String tempoDecorrido(Date date) {
-		Calendar cAtual = Calendar.getInstance();
-		Calendar c = Calendar.getInstance();
+    public String tempoDecorrido(Date date) {
+        Calendar cAtual = Calendar.getInstance();
+        Calendar c = Calendar.getInstance();
         c.setTime(date);
-        int diferencaAnos = cAtual.get(Calendar.YEAR) - c.get(Calendar.YEAR); 
-        int diferencaMeses =  Math.abs(cAtual.get(Calendar.MONTH) - c.get(Calendar.MONTH));
-        int diferencaDias = Math.abs(cAtual.get(Calendar.DATE) - c.get(Calendar.DATE));
-        return String
-        		.format("%d ano(s), %d messe(s) e %d dia(s)", 
-        		diferencaAnos, diferencaMeses, diferencaDias);
-	}
+        long diff = cAtual.getTime().getTime() - c.getTime().getTime();
+       
+        long dataEmDias = TimeUnit.MILLISECONDS.toDays(diff);
+        long meses = dataEmDias%365 / 30;
+        long dias = Math.abs((dataEmDias%365)%31);
+        long anos = Math.abs(dataEmDias/365);
+        
+       return String
+                .format("%d ano(s), %d messe(s) e %d dia(s)",
+                        anos, meses, dias);
+    }
 
-	private DiaSemana descobrirDiaDaSemanaPorNumero (int numero) {
-		switch (numero) {
-		case 1:
-			return DiaSemana.DOMINGO;
-		case 2:
-			return DiaSemana.SEGUNDA_FEIRA;
-		case 3: 
-			return DiaSemana.TERCA_FEIRA;
-		case 4:
-			return DiaSemana.QUARTA_FEIRA;
-		case 5: 
-			return DiaSemana.QUINTA_FEIRA;
-		case 6:
-			return DiaSemana.SEXTA_FEIRA;
-		default:
-			return DiaSemana.SABADO;
-		}
-	}
-	
+    private DiaSemana descobrirDiaDaSemanaPorNumero(int numero) {
+        switch (numero) {
+            case 1:
+                return DiaSemana.DOMINGO;
+            case 2:
+                return DiaSemana.SEGUNDA_FEIRA;
+            case 3:
+                return DiaSemana.TERCA_FEIRA;
+            case 4:
+                return DiaSemana.QUARTA_FEIRA;
+            case 5:
+                return DiaSemana.QUINTA_FEIRA;
+            case 6:
+                return DiaSemana.SEXTA_FEIRA;
+            default:
+                return DiaSemana.SABADO;
+        }
+    }
+
 }
