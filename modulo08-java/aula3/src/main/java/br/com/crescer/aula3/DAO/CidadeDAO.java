aed27921a29adedc4404de709a3d225075234649
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-22 17:33:59 -0300

    feat(cidade): adicionar pojo, interface e implementação DAO de cidade.

---
+++ modulo08-java/aula3/src/main/java/br/com/crescer/aula3/DAO/CidadeDAO.java
@@ -0,0 +1,10 @@
+package br.com.crescer.aula3.DAO;
+
+import br.com.crescer.aula3.POJO.Cidade;
+
+/*
+ * @author carloshenrique
+ */
+public interface CidadeDAO extends DAO<Cidade> {
+
+}
---
+++ modulo08-java/aula3/src/main/java/br/com/crescer/aula3/POJO/Cidade.java
@@ -0,0 +1,40 @@
+package br.com.crescer.aula3.POJO;
+
+/**
+ * @author carloshenrique
+ */
+public class Cidade {
+
+    private Long id;
+
+    private String nome;
+    
+    private Long estado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Long estado) {
+        this.estado = estado;
+    }
+
+   
+
+}
