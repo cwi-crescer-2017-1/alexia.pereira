fad065b3443d83fccf3a2a74866e71236ba86073
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-22 17:34:43 -0300

    feat(run): adicionar testes manuais de pais.

--- modulo08-java/aula3/src/main/java/br/com/crescer/aula3/Run.java
+++ modulo08-java/aula3/src/main/java/br/com/crescer/aula3/Run.java
@@ -1,10 +1,9 @@
 package br.com.crescer.aula3;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import br.com.crescer.aula3.DAO.PaisDAO;
+import br.com.crescer.aula3.DAO.PaisDAOImpl;
+import br.com.crescer.aula3.POJO.Pais;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /*
  * @author alexia.pereira
@@ -12,9 +11,19 @@ import java.sql.Statement;
 public class Run {
 
     public static void main(String[] args) throws SQLException {
-        TesteDAO dao = new TesteDAO();
-        dao.drop();
-        dao.create();
-        dao.insert();
+        //PAIS
+        PaisDAO paisDAO = new PaisDAOImpl();
+        Pais eua = new Pais();
+        //INSERT
+        eua.setId(new Long(2));
+        eua.setNome("Estados Unidos 2");
+        eua.setSigla("EUA");
+        //paisDAO.insert(eua);
+        //LOAD
+        System.out.println(paisDAO.loadBy(eua.getId()).getNome());
+        //UPDATE
+        paisDAO.update(eua);
+        //DELETE
+        paisDAO.delete(eua);
     }
 }
