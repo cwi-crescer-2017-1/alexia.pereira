c9ff057a431e60623737b8d3888bd8e6bf2542f8
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-30 17:35:24 -0300

    feat(usuario): enviar usuario da sessão atual.

--- modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Controller/LoggedUserController.java
+++ modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Controller/LoggedUserController.java
@@ -1,6 +1,9 @@
 package br.com.crescer.redesocial.Controller;
 
+import br.com.crescer.redesocial.Entity.Usuario;
+import br.com.crescer.redesocial.Repository.UsuarioRepository;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(LoggedUserController.PATH)
 public class LoggedUserController {
 
+    @Autowired
+    UsuarioRepository repository;
+
     public static final String PATH = "/usuarioAtual";
 
     @GetMapping
-    public User getUserDetails(Authentication authentication) {
+    public Usuario getUserDetails(Authentication authentication) {
         return Optional.ofNullable(authentication)
                 .map(Authentication::getPrincipal)
                 .map(User.class::cast)
+                .map(User::getUsername)
+                .map(repository::findOneByEmail)
                 .orElse(null);
     }
 }
