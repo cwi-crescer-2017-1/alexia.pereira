ac08f442e5c6fe3ab4cd12eec3bceaeecc6b3f39
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-29 23:07:52 -0300

    refactor(run): remover implementação de código erroneamente implementada.

---
+++ modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Run.java
@@ -0,0 +1,17 @@
+package br.com.crescer.redesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Carlos H. Nonnemacher
+ */
+@SpringBootApplication
+public class Run {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Run.class, args);
+        
+    }
+
+}
