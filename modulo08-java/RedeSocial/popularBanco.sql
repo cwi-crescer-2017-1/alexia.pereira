54e1a6a0e0988fab768fa744e8d2c8c6c5bd0d1c
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-06-30 00:34:47 -0300

    feat(popularBanco): adicionar script para adicionar dados no banco.

---
+++ modulo08-java/RedeSocial/popularBanco.sql
@@ -0,0 +1,9 @@
+INSERT INTO USUARIO (NOME, SEXO, DATA_NASCIMENTO, EMAIL, SENHA, CASA)
+VALUES ('Harry Potter', 'M', TO_DATE('31/07/1990'), 'harry@potter', 'hp', 'Grifinória');
+INSERT INTO USUARIO (NOME, SEXO, DATA_NASCIMENTO, EMAIL, SENHA, CASA)
+VALUES ('Hermione Granger', 'F', TO_DATE('19/09/1989'), 'hermione@granger', 'hg', 'Grifinória');
+INSERT INTO USUARIO (NOME, SEXO, DATA_NASCIMENTO, EMAIL, SENHA, CASA)
+VALUES ('Draco Malfoy', 'M', TO_DATE('05/07/1990'), 'draco@malfoy', 'dm', 'Sonserina');
+INSERT INTO USUARIO (NOME, SEXO, DATA_NASCIMENTO, EMAIL, SENHA, CASA)
+VALUES ('Luna Lovegood', 'F', TO_DATE('13/02/1990'), 'luna@lovegood', 'll', 'Corvinal');
+COMMIT;
