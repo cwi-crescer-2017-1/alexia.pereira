61831863f32304a753ff95506f58ec973590cce5
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-07-01 23:04:58 -0300

    feat(solicitacao): adicionar metodo de busca por usuario alvo.

--- modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Controller/SolicitacaoController.java
+++ modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Controller/SolicitacaoController.java
@@ -1,10 +1,15 @@
 package br.com.crescer.redesocial.Controller;
 
 import br.com.crescer.redesocial.Entity.Solicitacao;
+import br.com.crescer.redesocial.Entity.Usuario;
 import br.com.crescer.redesocial.Service.SolicitacaoService;
+import br.com.crescer.redesocial.Service.UsuarioService;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +24,31 @@ public class SolicitacaoController {
 
     @Autowired
     SolicitacaoService service;
+    
+    @Autowired
+    UsuarioService usuarioService;
 
     @PostMapping
     public Solicitacao save(@RequestBody Solicitacao solicitacao) {
         solicitacao.setIdSolicitacao(0l);
         return service.enviarSolicitacao(solicitacao);
     }
-    
-    
-    @PostMapping(value="/aceitar")
+
+    @PostMapping(value = "/aceitar")
     public void accept(@RequestBody Solicitacao solicitacao) {
         service.aceitarSolicitacao(solicitacao);
     }
     
+    @DeleteMapping(value="/delete/{idSolicitacao}")
+    public void delete (@PathVariable Long idSolicitacao) {
+        Solicitacao solicitacao = service.buscarPorId(idSolicitacao);
+        service.remover(solicitacao);
+    }
+
+    @GetMapping(value = "/pendentes/{idUsuario}")
+    public Set<Solicitacao> pendentes(@PathVariable Long idUsuario) {
+        Usuario usuarioTarget = usuarioService.loadById(idUsuario);
+        return service.buscarSolicitacoesPorUsuario(usuarioTarget);
+    }
+
 }
--- modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Repository/SolicitacaoRepository.java
+++ modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Repository/SolicitacaoRepository.java
@@ -1,6 +1,8 @@
 package br.com.crescer.redesocial.Repository;
 
 import br.com.crescer.redesocial.Entity.Solicitacao;
+import br.com.crescer.redesocial.Entity.Usuario;
+import java.util.Set;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
@@ -8,5 +10,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * @author A
  */
 public interface SolicitacaoRepository extends PagingAndSortingRepository<Solicitacao, Long> {
-    
+    Set<Solicitacao> findByUsuarioTarget(Usuario usuarioTarget);
 }
