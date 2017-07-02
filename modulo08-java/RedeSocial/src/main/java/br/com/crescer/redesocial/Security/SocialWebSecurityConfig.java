eee192f6a4a9b489a0842a1f971ab1ceca8a1197
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-07-01 23:02:11 -0300

    feat(cors): adicionar liberação para a porta 8080 na aplicação.

--- modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Security/SocialWebSecurityConfig.java
+++ modulo08-java/RedeSocial/src/main/java/br/com/crescer/redesocial/Security/SocialWebSecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.crescer.redesocial.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author alexia.pereira
  */
 @EnableWebSecurity
+@Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SocialWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -51,7 +53,9 @@ public class SocialWebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("PUT", "DELETE", "GET", "POST");
             }
         };
     }
