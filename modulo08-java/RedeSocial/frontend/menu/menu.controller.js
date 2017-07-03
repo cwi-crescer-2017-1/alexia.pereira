0e5ece859d08fb546fe475dc58ee9e9015b63b4d
Alexia Pereira <alexia.pereira@cwi.com.br>
2017-07-03 17:19:23 -0300

    feat(notificacoes): adicionar notificacoes ao menu.

--- modulo08-java/RedeSocial/frontend/menu/menu.controller.js
+++ modulo08-java/RedeSocial/frontend/menu/menu.controller.js
@@ -1,7 +1,14 @@
-angular.module('app').controller('MenuController', function ($scope, $location, authService) {
+angular.module('app').controller('MenuController', function ($scope, $location, authService,
+  solicitacaoService, Alertify) {
+
   $scope.usuario = authService.getUsuario();
   $scope.dashboard = dashboard;
   $scope.amigos = amigos;
+  $scope.notificacoes = false;
+  $scope.exibirNotificacoes = exibirNotificacoes;
+  $scope.aceitarSolicitacao = aceitarSolicitacao;
+  $scope.recusarSolicitacao = recusarSolicitacao;
+  buscarSolicitacoes();
 
   function dashboard() {
     $location.path('/dashboard');
@@ -10,5 +17,30 @@ angular.module('app').controller('MenuController', function ($scope, $location, 
   function amigos() {
     $location.path('/amigos');
   }
+
+  function exibirNotificacoes() {
+    $scope.notificacoes = $scope.notificacoes ? false : true;
+  }
+
+  function buscarSolicitacoes() {
+    solicitacaoService.getSolicitacoesPendentes(authService.getUsuario().idUsuario)
+    .then(function (response) {
+      $scope.solicitacoes = response.data;
+    });
+  }
+
+  function recusarSolicitacao(solicitacao) {
+    solicitacaoService.deletar(solicitacao.idSolicitacao)
+    .then(function (response) {
+      buscarSolicitacoes();
+    });
+  }
+
+  function aceitarSolicitacao(solicitacao) {
+    solicitacaoService.aceitar(solicitacao).then(function (response) {
+      buscarSolicitacoes();
+    })
+  }
+
 
 });
