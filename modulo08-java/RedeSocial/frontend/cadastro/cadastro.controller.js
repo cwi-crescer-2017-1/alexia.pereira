d13c9db011feaa627a825fa9d29c049e5a12da00
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-04 03:08:21 -0300

    feat(dependencia): adicionar dependencia para factory injetada.

--- modulo08-java/RedeSocial/frontend/cadastro/cadastro.controller.js
+++ modulo08-java/RedeSocial/frontend/cadastro/cadastro.controller.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.controller('CadastroController', function ($scope, $location, usuarioService, authService) {
+.controller('CadastroController', function ($scope, $location, usuarioService, authService, Alertify) {
 
   $scope.sexos = [
     {
