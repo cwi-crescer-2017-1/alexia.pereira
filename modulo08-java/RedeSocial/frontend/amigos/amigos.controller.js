f7e71220a564362fbfbddaa2a2892a25f0116228
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-04 02:56:21 -0300

    feat(perfilRedirecionamento): redirecionar para perfil de usuario.

--- modulo08-java/RedeSocial/frontend/amigos/amigos.controller.js
+++ modulo08-java/RedeSocial/frontend/amigos/amigos.controller.js
@@ -1,12 +1,17 @@
 angular.module('app').controller('AmigosController', function ($scope, $location, authService, usuarioService) {
 
   listar();
+  $scope.verPerfil = verPerfil;
 
   function listar () {
     usuarioService.buscarAmigos(authService.getUsuario().idUsuario).then(response => {
       $scope.usuario = response.data;
       $scope.amigos = $scope.usuario.amigos;
     })
+  }
+
+  function verPerfil (usuario) {
+    $location.path('/usuario/' + usuario.idUsuario);
   }
 
 });
