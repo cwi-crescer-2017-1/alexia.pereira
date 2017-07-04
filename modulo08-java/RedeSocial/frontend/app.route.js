5814c162880d3b5d27b2b7d2b87e1bcc688c3243
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-04 00:35:50 -0300

    feat(busca): adicionar tela de busca de usuarios.

--- modulo08-java/RedeSocial/frontend/app.route.js
+++ modulo08-java/RedeSocial/frontend/app.route.js
@@ -18,6 +18,16 @@ angular.module('app').config(function ($routeProvider) {
         }
       }
     })
+    .when('/busca', {
+        controller: 'BuscaController',
+        templateUrl: 'busca/busca.html',
+        resolve: {
+          // define que para acessar esta página deve ser um usuário autenticado (mas não restringe o tipo de permissão)
+          autenticado: function (authService) {
+            return authService.isAutenticadoPromise();
+          }
+        }
+      })
     .when('/amigos', {
         controller: 'AmigosController',
         templateUrl: 'amigos/amigos.html',
---
+++ modulo08-java/RedeSocial/frontend/busca/busca.controller.js
@@ -0,0 +1,70 @@
+angular.module('app').controller('BuscaController', function ($location, $scope,
+  usuarioService, authService, solicitacaoService, Alertify) {
+    $scope.buscar = buscar;
+    $scope.adicionarAosAmigos = adicionarAosAmigos;
+    $scope.enviarSolicitacao = enviarSolicitacao;
+    buscarUsuarioAtual();
+    buscarSolicitacoes();
+    buscarMinhasSolicitacoes();
+
+    function buscar (nome) {
+      usuarioService.buscarPorNome(nome).then(res => {
+        $scope.usuarios = res.data;
+      })
+    }
+
+    function adicionarAosAmigos(usuario) {
+      if (jaSomosAmigos(usuario) || jaMeAdicionou(usuario) || jaTeAdicionei(usuario) || souEu(usuario)) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    function buscarUsuarioAtual() {
+      usuarioService.buscarAmigos(authService.getUsuario().idUsuario).then(response => {
+        $scope.usuario = response.data;
+        $scope.amigos = $scope.usuario.amigos;
+      });
+    }
+
+    function buscarSolicitacoes() {
+      solicitacaoService.getSolicitacoesPendentes(authService.getUsuario().idUsuario)
+      .then(function (response) {
+        $scope.solicitacoes = response.data;
+      });
+    }
+
+    function buscarMinhasSolicitacoes() {
+      solicitacaoService.getMinhasSolicitacoes(authService.getUsuario().idUsuario)
+      .then(function (response) {
+        $scope.minhasSolicitacoes = response.data;
+      });
+    }
+
+    function jaSomosAmigos(usuario) {
+      return $scope.usuario.amigos.map(a => a.idUsuario).indexOf(usuario.idUsuario) !== -1;
+    }
+
+    function jaMeAdicionou(usuario) {
+      return $scope.solicitacoes.map(s => s.usuarioOwner.idUsuario).indexOf(usuario.idUsuario) !== -1;
+    }
+
+    function jaTeAdicionei(usuario) {
+      return $scope.minhasSolicitacoes.map(s => s.usuarioTarget.idUsuario).indexOf(usuario.idUsuario) !== -1;
+    }
+
+    function souEu(usuario) {
+      return usuario.idUsuario === $scope.usuario.idUsuario;
+    }
+
+    function enviarSolicitacao(usuario) {
+      let solicitacao = {usuarioOwner: $scope.usuario, usuarioTarget: usuario};
+      solicitacaoService.criar(solicitacao).then(res => {
+        Alertify.success('Solicitação enviada com sucesso');
+        buscarMinhasSolicitacoes();
+        buscar($scope.nomeUser);
+      });
+    }
+
+  });
