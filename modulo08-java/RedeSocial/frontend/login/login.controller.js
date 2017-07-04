a64eae77a24fc0fca0dcf678a0ef58c1b7705c51
AlexiaPereira <alexia.pereira@cwi.com.br>
2017-07-03 21:05:33 -0300

    feat(frontend-redirecionamento): redirecionar usuario para dashboard caso logado.

--- modulo08-java/RedeSocial/frontend/login/login.controller.js
+++ modulo08-java/RedeSocial/frontend/login/login.controller.js
@@ -2,23 +2,27 @@ angular.module('app').controller('LoginController', function ($location, $scope,
 
   $scope.login = login;
   $scope.telaCadastro = telaCadastro;
+  verificarUsuarioLogado();
 
   function login (usuario) {
     authService.login(usuario)
     .then(
       function (response) {
-        console.log(response);
         alert('Login com sucesso!');
 
       },
       function (response) {
-        console.log(response);
         alert('Erro no Login!');
       });
     }
 
     function telaCadastro() {
       $location.path('/cadastro');
+    }
+
+    function verificarUsuarioLogado() {
+      if (authService.isAutenticado())
+        $location.path('/dashboard');
     }
 
   });
